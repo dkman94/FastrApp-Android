1a5929a962d81de0f74b16a58606031839fd9473
dkman94 <deepak.s.kumar.85@gmail.com>
2019-11-01 20:55:09 -0400

    initial swipeable view pager

---
+++ app/src/main/java/app/android/fastrapp/SmileTestFragment.kt
@@ -0,0 +1,15 @@
+package app.android.fastrapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class SmileTestFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = inflater.inflate(R.layout.smile_tests_page_fragment, container, false)
+}
