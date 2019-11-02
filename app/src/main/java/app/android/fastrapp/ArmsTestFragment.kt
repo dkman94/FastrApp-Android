abbf59c09422cedf53f46361b7f4aa4f154451eb
Jonathan <tiaojon@umich.edu>
2019-11-02 10:38:32 -0400

    layout/skeleton for arms test

--- app/src/main/java/app/android/fastrapp/ArmsTestFragment.kt
+++ app/src/main/java/app/android/fastrapp/ArmsTestFragment.kt
@@ -3,13 +3,27 @@ package app.android.fastrapp
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.TextView
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.arms_test_page_fragment.view.*
 
 class ArmsTestFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View = inflater.inflate(R.layout.arms_test_page_fragment, container, false)
+    ): View {
+        val view= inflater.inflate(R.layout.arms_test_page_fragment, container, false)
+        val yesBtn = view.findViewById<TextView>(R.id.yes_weakness);
+        yesBtn.setOnClickListener(){
+
+        }
+        val noBtn = view.findViewById<TextView>(R.id.no_weakness);
+        noBtn.setOnClickListener(){
+
+        }
+        return view;
+    }
 }
