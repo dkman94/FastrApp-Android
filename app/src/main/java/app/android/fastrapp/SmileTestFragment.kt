953bd8a4a963c1f71dbb98937a147987880ef8b7
dkman94 <deepak.s.kumar.85@gmail.com>
2019-11-01 23:45:21 -0400

    add camera and face detection api

--- app/src/main/java/app/android/fastrapp/SmileTestFragment.kt
+++ app/src/main/java/app/android/fastrapp/SmileTestFragment.kt
@@ -1,15 +1,111 @@
 package app.android.fastrapp
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Matrix
+import android.os.Build
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.util.Size
+import android.view.*
+import androidx.annotation.RequiresApi
+import androidx.camera.core.CameraX
+import androidx.camera.core.Preview
+import androidx.camera.core.PreviewConfig
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetectorOptions
+
+private const val REQUEST_CODE_PERMISSIONS = 10
+private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
 
 class SmileTestFragment : Fragment() {
+
+    lateinit var viewFinder: TextureView
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View = inflater.inflate(R.layout.smile_tests_page_fragment, container, false)
+    ): View {
+        val view = inflater.inflate(R.layout.smile_tests_page_fragment, container, false)
+
+        viewFinder = view.findViewById<TextureView>(R.id.view_finder)
+        viewFinder.post {
+            if (allPermissionsGranted()) {
+                viewFinder.post { startCamera() }
+            } else {
+                requestPermissions(REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+                println("here")
+            }
+        }
+        viewFinder.addOnLayoutChangeListener { _, _, _, _, _, _, _, _, _ ->
+            updateTransform(viewFinder)
+        }
+
+        val options = FirebaseVisionFaceDetectorOptions.Builder().apply {
+            setPerformanceMode(FirebaseVisionFaceDetectorOptions.ACCURATE)
+            setLandmarkMode(FirebaseVisionFaceDetectorOptions.ALL_LANDMARKS)
+            setMinFaceSize(.15f)
+        }
+
+
+        return view
+    }
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    private fun startCamera() {
+
+        // Create configuration object for the viewfinder use case
+        val previewConfig = PreviewConfig.Builder().apply {
+            setTargetResolution(Size(640, 480))
+        }.build()
+
+
+        // Build the viewfinder use case
+        val preview = Preview(previewConfig)
+
+        // Every time the viewfinder is updated, recompute layout
+        preview.setOnPreviewOutputUpdateListener {
+
+            // To update the SurfaceTexture, we have to remove it and re-add it
+            val parent = viewFinder.parent as ViewGroup
+            parent.removeView(viewFinder)
+            parent.addView(viewFinder, 0)
+
+            viewFinder.surfaceTexture = it.surfaceTexture
+            updateTransform(viewFinder)
+        }
+
+        CameraX.bindToLifecycle(this, preview)
+
+    }
+
+    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+    private fun updateTransform(viewFinder: TextureView) {
+        val matrix = Matrix()
+
+        // Compute the center of the view finder
+        val centerX = viewFinder.width / 2f
+        val centerY = viewFinder.height / 2f
+
+        // Correct preview output to account for display rotation
+        val rotationDegrees = when (viewFinder.display.rotation) {
+            Surface.ROTATION_0 -> 0
+            Surface.ROTATION_90 -> 90
+            Surface.ROTATION_180 -> 180
+            Surface.ROTATION_270 -> 270
+            else -> return
+        }
+        matrix.postRotate(-rotationDegrees.toFloat(), centerX, centerY)
+
+        // Finally, apply transformations to our TextureView
+        viewFinder.setTransform(matrix)
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            context!!, it
+        ) == PackageManager.PERMISSION_GRANTED
+    }
 }
