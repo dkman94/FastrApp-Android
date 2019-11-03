7d62bc690e1d1c003fa46d5d15d8b8f8b3979810
Alex Markovits <amarkovits@oneconcern.com>
2019-11-02 21:43:14 -0400

    word error rate
    Signed-off-by: Alex Markovits <amarkovits@oneconcern.com>

--- app/src/main/java/app/android/fastrapp/SpeakTestFragment.kt
+++ app/src/main/java/app/android/fastrapp/SpeakTestFragment.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.speech.RecognizerIntent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,7 +13,9 @@ import android.widget.ImageButton
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.pwnetics.metric.WordSequenceAligner
 import java.util.*
+
 
 class SpeakTestFragment : Fragment() {
     lateinit var helper: UserPrefsHelper
@@ -23,7 +26,7 @@ class SpeakTestFragment : Fragment() {
 
     private val REQ_CODE = 100
     private val RESULT_OK = 200
-    private val SPEAK_TEST_CASE = "Alex Caruso is the greatest basketball player"
+    private val WER_THRESHOLD = 0.25
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -66,10 +69,12 @@ class SpeakTestFragment : Fragment() {
                     RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
                 )
                 intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
-                intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Please repeat: " + SPEAK_TEST_CASE)
+                val testSentence = getString(R.string.speech_test_case)
+                intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Please repeat: '$testSentence'")
                 startActivityForResult(intent, REQ_CODE)
             }
         }
+
         return view
     }
 
@@ -79,8 +84,19 @@ class SpeakTestFragment : Fragment() {
             REQ_CODE -> {
                 if (data != null) {
                     val result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
-                    if (!result.equals(SPEAK_TEST_CASE)) {
-                        Toast.makeText(context!!, R.string.fail, Toast.LENGTH_LONG).show()
+                    val test_case = getString(R.string.speech_test_case).split(" ").toTypedArray()
+                    for (r in result) {
+                        val wsa = WordSequenceAligner()
+                        val arrayResult = r.split(" ").toTypedArray()
+                        val alignment = wsa.align(arrayResult, test_case)
+                        val wer = wsa.SummaryStatistics(listOf(alignment)).wordErrorRate
+
+                        Log.d("alignment", alignment.toString())
+                        Log.d("wer", wer.toString())
+
+                        if (wer < WER_THRESHOLD) {
+                            // TODO
+                        }
                     }
                 }
             }
