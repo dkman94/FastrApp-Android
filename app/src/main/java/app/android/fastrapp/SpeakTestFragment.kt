829cc74fd683d48ee5385a09254f7e3a746ec2d6
dkman94 <deepak.s.kumar.85@gmail.com>
2019-11-02 20:03:25 -0400

    speech stuff mostly working

--- app/src/main/java/app/android/fastrapp/SpeakTestFragment.kt
+++ app/src/main/java/app/android/fastrapp/SpeakTestFragment.kt
@@ -1,42 +1,33 @@
 package app.android.fastrapp
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.speech.RecognizerIntent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-
-import android.Manifest
-import android.content.pm.PackageManager
 import android.widget.ImageButton
-import androidx.core.content.ContextCompat
-import java.io.IOException
-import android.speech.RecognizerIntent
-import android.content.Intent
-//import sun.jvm.hotspot.utilities.IntArray
 import android.widget.Toast
-//import androidx.test.core.app.ApplicationProvider.getApplicationContext
-import android.content.ActivityNotFoundException
-import android.content.Context
-import android.util.Log
-//import sun.jvm.hotspot.utilities.IntArray
-//import androidx.test.core.app.ApplicationProvider.getApplicationContext
-//import sun.jvm.hotspot.utilities.IntArray
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import java.util.*
+
+
 //import sun.jvm.hotspot.utilities.IntArray
-
-
 
 
 class SpeakTestFragment : Fragment() {
-    lateinit var helper:UserPrefsHelper
+    lateinit var helper: UserPrefsHelper
 
-    fun initialize(helper: UserPrefsHelper){
+    fun initialize(helper: UserPrefsHelper) {
         this.helper = helper
     }
 
     private val REQ_CODE = 100
     private val RESULT_OK = 200
+    private val SPEAK_TEST_CASE = "Alex Caruso is the greatest basketball player"
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -52,18 +43,20 @@ class SpeakTestFragment : Fragment() {
         )
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
         intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Need to speak")
-
         val toggleRecordingButton = view.findViewById<ImageButton>(R.id.mic_button)
 
         toggleRecordingButton.setOnClickListener {
             if (ContextCompat.checkSelfPermission(
                     context!!,
-                    Manifest.permission.RECORD_AUDIO)
+                    Manifest.permission.RECORD_AUDIO
+                )
                 != PackageManager.PERMISSION_GRANTED &&
                 ContextCompat.checkSelfPermission(
                     context!!,
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED) {
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                )
+                != PackageManager.PERMISSION_GRANTED
+            ) {
                 val permissions = arrayOf(
                     Manifest.permission.RECORD_AUDIO,
                     Manifest.permission.WRITE_EXTERNAL_STORAGE,
@@ -71,42 +64,28 @@ class SpeakTestFragment : Fragment() {
                 )
                 requestPermissions(permissions, 0)
             } else {
-                startRecording(intent)
+                val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+                intent.putExtra(
+                    RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                    RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+                )
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+                intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Please repeat: " + SPEAK_TEST_CASE)
+                startActivityForResult(intent, REQ_CODE)
             }
         }
-
-//        toggleRecordingButton.setOnHoverListener {
-//            toggleRecordingButton.setBackgroundColor(
-//                ContextCompat.getColor(this, R.color.colorLightBlue2)
-//            )
-//        }
-
         return view
-    }
-
-    private fun startRecording(intent: Intent) {
-        try {
-            startActivityForResult(intent, REQ_CODE)
-        } catch (a: ActivityNotFoundException) {
-            Toast.makeText(
-                context!!,
-                "Sorry your device not supported",
-                Toast.LENGTH_SHORT
-            ).show()
-        } catch (e: IllegalStateException) {
-            e.printStackTrace()
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         when (requestCode) {
             REQ_CODE -> {
-                if (resultCode == RESULT_OK && data != null) {
+                if (data != null) {
                     val result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
-                    Log.d("result: ", result.get(0))
+                    if (!result.equals(SPEAK_TEST_CASE)) {
+                        Toast.makeText(context!!, R.string.fail, Toast.LENGTH_LONG).show()
+                    }
                 }
             }
         }
