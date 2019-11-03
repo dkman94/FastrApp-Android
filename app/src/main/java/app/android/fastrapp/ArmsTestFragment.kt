a8f20747154bea43e82fb2a61dffb30d98cf8f44
Deepak Kumar <deepak.s.kumar.85@gmail.com>
2019-11-02 23:32:32 -0400

    Fix smile api call (#20)
    * add arms raised and update string
    
    * fix arms ui
    
    * fix other issues

--- app/src/main/java/app/android/fastrapp/ArmsTestFragment.kt
+++ app/src/main/java/app/android/fastrapp/ArmsTestFragment.kt
@@ -3,33 +3,35 @@ package app.android.fastrapp
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
-import android.widget.TextView
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.Toast
+import android.widget.TextView
 import androidx.fragment.app.Fragment
-import kotlinx.android.synthetic.main.arms_test_page_fragment.view.*
 
 class ArmsTestFragment : Fragment() {
-    lateinit var helper:UserPrefsHelper;
+    lateinit var helper: UserPrefsHelper;
 
-    fun initialize(helper: UserPrefsHelper){
+    fun initialize(helper: UserPrefsHelper) {
         this.helper = helper
     }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val view= inflater.inflate(R.layout.arms_test_page_fragment, container, false)
+        val view = inflater.inflate(R.layout.arms_test_page_fragment, container, false)
         val yesBtn = view.findViewById<TextView>(R.id.yes_weakness);
-        yesBtn.setOnClickListener(){
-            helper.UpdateActivity(UserPrefsHelper.TestType.ARM, true)
-            //helper.WriteActivity() example of how to call WriteActivity
+        val armsResult = view.findViewById<TextView>(R.id.arms_test_result)
+        yesBtn.setOnClickListener {
+            helper.UpdateActivity(UserPrefsHelper.TestType.ARM, false)
+            armsResult.setText(R.string.no_arm_weakness)
+            armsResult.setBackgroundColor(context!!.resources.getColor(R.color.healthyTestAlert))
         }
         val noBtn = view.findViewById<TextView>(R.id.no_weakness);
-        noBtn.setOnClickListener(){
-            helper.UpdateActivity(UserPrefsHelper.TestType.ARM, false)
+        noBtn.setOnClickListener {
+            helper.UpdateActivity(UserPrefsHelper.TestType.ARM, true)
+            armsResult.setText(R.string.arms_weakness)
+            armsResult.setBackgroundColor(context!!.resources.getColor(R.color.failedTestAlert))
         }
         return view;
     }
